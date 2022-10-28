c25f89cce9bba31a82850e854162503b11638f32
AnGgIt88 <jarbull87@gmail.com>
2022-10-28 07:23:51 +0000

    Geblek

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -7,40 +7,26 @@ msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
 
-# Telegram
-tg_check=$(grep 'TG' $CONFIG | wc -l)
-if [[ $tg_check -gt 0 ]]; then
-    echo "ERROR - Anda Tidak Dapat Menyetel *TG* Vars di $CONFIG"
-    exit 1
-fi
-
-# Do not allow curl
-curl_check=$(grep 'curl ' $CONFIG | wc -l)
-if [[ $curl_check -gt 0 ]]; then
-    echo -e "Tolong jangan gunakan \'curl\' di $CONFIG".
-    exit 1
-fi
-
 if [[ "$DEVICE" == "" ]]; then
     echo "Environment untuk 'DEVICE' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$ORANGEFOX_MAINTAINER_NAME" == "" ]]; then
-    echo "Environment untuk 'ROM_NAME' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'ORANGEFOX_MAINTAINER_NAME' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$FOX_BRANCH" == "" ]]; then
-    echo "Environment untuk 'BRANCH_MANIFEST' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'FOX_BRANCH' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$DT_LINK" == "" ]]; then
-    echo "Environment untuk 'TG_TOKEN' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'DT_LINK' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$DT_BRANCH" == "" ]]; then
-    echo "Environment untuk 'CHAT_ID' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'DT_BRANCH' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$TARGET" == "" ]]; then
-    echo "Environment untuk 'G_H_TOKEN' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'TARGET' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 elif [[ "$OUTPUT" == "" ]]; then
-    echo "Environment untuk 'BUILDTYPE' kosong, silakan atur dengan mengedit $CONFIG"
+    echo "Environment untuk 'OUTPUT' kosong, silakan atur dengan mengedit $CONFIG"
     exit 1
 else
     echo "Semua akan baik baik saja hehe!"
